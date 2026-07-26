1ee6a8ff4a7a7d90ceb8f41fb80fe540fbf57af9
Mayank <mayank.a125052@gmail.com>
2026-07-26 20:01:00 +0530

    minor changes

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
+
 // Algorithms
 export { FixedWindow } from "./algorithms/fixed-window.js";
 export { LeakyBucket } from "./algorithms/leaky-bucket.js";
 export { SlidingWindowLog } from "./algorithms/sliding-window-log.js";
 export { TokenBucket } from "./algorithms/token-bucket.js";
+export { SlidingWindowCounter } from "./algorithms/sliding-window-counter.js";
+export { GCRA } from "./algorithms/gcra.js";
 
 // Rate Limiter
 export { RateLimiter } from "./core/RateLimiter.js";
 
 // Stores
 export { MemoryStore } from "./stores/memoryStore.js";
+export { RedisStore } from "./stores/redisStore.js";
+export { MongoStore } from "./stores/mongoStore.js";
+export { PostgresStore } from "./stores/postgresStore.js";
 
 
 // Types
